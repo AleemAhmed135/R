97e64dacf70fb0080d1b8820077c02a136bdf7c1
AleemAhmed135 <113816495+AleemAhmed135@users.noreply.github.com>
2023-01-28 11:37:29 +0530

    Add files via upload

---
+++ sessionalR/R/sessinal.R
@@ -0,0 +1,7 @@
+library("devtools")
+install_github("AleemAhmed135/R/TESTPACKAGE")
+x <- c(10,20,30,40,50,60,70,80,90,100)
+prog(x)
+#running after changes
+install_github("AleemAhmed135/R/sessionalR")
+prog(x)
